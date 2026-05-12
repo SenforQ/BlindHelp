303cd0b6ddac9a5e576dc780a0e6c53d5e857d58
zcq <494313298@qq.com>
2026-05-12 16:57:06 +0800

    init

---
+++ BlindHelp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlindHelp
+//
+//  Created by tabier on 2026/5/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
